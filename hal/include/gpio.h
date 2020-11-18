5d71c9e72ddcecb59b211e5747e0de5cdd3da57a
jay zhou <jay.zhou@ihoment.com>
2020-11-18 01:19:14 -0800

    feat(gpio): add gpio's config

---
+++ hal/include/gpio.h
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  gpio.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/17/2020 10:54:13 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef  gpio_INC
+#define  gpio_INC
+void gpio_pmu_wakeupsrc(unsigned int gpios);
+#endif   /* ----- #ifndef gpio_INC  ----- */
