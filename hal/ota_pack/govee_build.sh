7d6b97561e12eaba4fef53e4507434673ae5aa89
ejuoy <84949657@qq.com>
2020-12-25 17:37:43 +0800

    rm makefile \r

--- hal/ota_pack/govee_build.sh
+++ hal/ota_pack/govee_build.sh
@@ -5,6 +5,7 @@ prj_path=$(pwd)
 
 frq_sdk_path_line=$(cat Makefile| grep ^SDK_ROOT)
 frq_sdk_path_=${frq_sdk_path_line#*=}
+frq_sdk_path_=`eval echo $frq_sdk_path_|tr -d '\\r'`
 frq_sdk_path=`eval echo $frq_sdk_path_`
 pack_file_path="/../hal/ota_pack"
 
@@ -18,7 +19,14 @@ cp -r $frq_sdk_path$pack_file_path_c_tail $frq_sdk_path$ota_file_path_c_tail
 cp -r $frq_sdk_path$pack_file_path_h_tail $frq_sdk_path$ota_file_path_h_tail
 
 #build frq
-make clean && make
+#build frq
+if [ $# != 1 ];then
+    echo "make"
+    make
+else
+    echo "make clean && make"
+    make clean&&make
+fi
 
 prj_file_os_head="./build/"
 prj_file_os_tail='.bin'
@@ -26,6 +34,7 @@ prj_file_os_tail='.bin'
 prj_name_find_line=$(cat Makefile| grep ^TARGETS)
 prj_file_name_=${prj_name_find_line#*=}
 prj_file_name=`eval echo $prj_file_name_`
+prj_file_name=`eval echo $prj_file_name|tr -d '\\r'`
 prj_file_os_name=$prj_file_os_head$prj_file_name$prj_file_os_tail
 
 # add crc
