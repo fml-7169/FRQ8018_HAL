670a8f9a844317e685e1ab3d8ddacb24e77a32ec
ejuoy <84949657@qq.com>
2021-03-17 17:52:32 +0800

    hal add uart0 uart1

--- hal_code/uart/middle_uart.h
+++ hal_code/uart/middle_uart.h
@@ -14,17 +14,23 @@
 
 typedef void (*uart_callback)(uint8* p_data, uint32 size);
 
+enum _mid_uart_id
+{
+    UART_ID_0,
+    UART_ID_1,
+    UART_MAX
+};
+
 typedef struct
 {
     int8 baud_rate;
     uart_callback uart_read;
 } uart_config_t;
 
-int32 mid_uart_command_get(uint8* buffer);
-int32 mid_uart_data_size(void);
-int mid_uart_data_get(uint8* buffer, uint32 size);
-int32 mid_uart_data_send(uint8* p_data, uint32 size);
-int32 mid_uart_init(int8 baud_rate);
-void mid_uart_port_set(Pin_Map tx_pin,Pin_Map rx_pin);
+int32 mid_uart_data_size(uint8 u_id);
+int mid_uart_data_get(uint8 u_id,uint8* buffer, uint32 size);
+int32 mid_uart_data_send(uint8 u_id,uint8* p_data, uint32 size);
+int32 mid_uart_init(uint8 u_id,int8 baud_rate);
+void mid_uart0_port_set(Pin_Map tx_pin,Pin_Map rx_pin);
 
 #endif /* MIDDLE_UART_H_ */
